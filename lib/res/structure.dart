ba644b424c176f231d49f8f3f6e03b7f9b55bf2d
Hans Daduya <hansdaduya@yahoo.com>
2023-02-24 14:15:47 +0800

    refactor maps file

--- lib/res/structure.dart
+++ lib/res/structure.dart
@@ -5,6 +5,7 @@ import '../authentication/email_password/email_password.dart' as email;
 import '../authentication/google_sign_in/google_sign_in.dart' as google;
 import '../authentication/phone_sign_in/phone_sign_in.dart' as phone;
 import '../backend_actions/user_presence/user_presence.dart' as user_presence;
+import '../maps/maps.dart' as maps;
 
 import 'custom_colors.dart';
 import 'fire_assets.dart';
@@ -210,7 +211,7 @@ List<Map<String, dynamic>> menu = [
           Icons.map_rounded,
           color: Palette.firebaseNavy,
         ),
-        'widget': null,
+        'widget': const maps.GoogleMaps()
       },
     ]
   },
